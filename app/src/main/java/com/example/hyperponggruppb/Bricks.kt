0341b038d0d017ed8eb64c23f36fc5055c5a24c1
LucaSalmi <luca.salmi90@gmail.com>
2021-11-30 12:57:03 +0100

    first commit logic for creating row of random coloured bricks ready

---
+++ app/src/main/java/com/example/hyperponggruppb/Bricks.kt
@@ -0,0 +1,9 @@
+package com.example.hyperponggruppb
+
+import android.graphics.Paint
+
+data class Bricks(var brickLeft: Float,var brickTop: Float,var brickRight: Float,var brickBottom: Float,var brickPaint: Paint) {
+
+
+
+}
