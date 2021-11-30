ea8c3e8464ed259ea4aa78925d12fbef050899b0
LucaSalmi <luca.salmi90@gmail.com>
2021-11-30 19:06:13 +0100

    primed binding and added fragments implementation

--- app/src/main/java/com/example/hyperponggruppb/MainActivity.kt
+++ app/src/main/java/com/example/hyperponggruppb/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.hyperponggruppb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hyperponggruppb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
