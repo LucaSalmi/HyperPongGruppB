58597d1f65769de75ac05ed7a61b2e249529d569
LucaSalmi <luca.salmi90@gmail.com>
2021-12-01 10:07:09 +0100

    code copied

--- app/src/main/java/com/example/hyperponggruppb/MainActivity.kt
+++ app/src/main/java/com/example/hyperponggruppb/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.hyperponggruppb
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentManager
@@ -32,5 +35,29 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    fun makeCanvas(){
+        val canvas: Canvas? = binding.surfaceView.holder.lockCanvas()
+        val background = Paint()
+        background.color = Color.GREEN
+
+        canvas?.drawRect(0f, 0f, binding.surfaceView.width.toFloat(), binding.surfaceView.height.toFloat(), background)
+
+        if (canvas != null) {
+
+            for(obj in brickRow){
+
+                canvas.drawRect(obj.brickLeft, obj.brickTop, obj.brickRight, obj.brickBottom, obj.brickPaint)
+            }
+
+        }
+
+
+
+
+
+        binding.surfaceView.holder.unlockCanvasAndPost(canvas)
+        binding.surfaceView.setZOrderOnTop(true)
     }
 }
