4096430e32bed8a745cbeb35f5e301b18da5547c
Roxmash <hampus.brandtman@student.kyh.se>
2021-12-16 12:47:46 +0100

    added a transperant bg on scoreBoard
    hb

--- app/src/main/java/com/example/hyperponggruppb/GameModeOneActivity.kt
+++ app/src/main/java/com/example/hyperponggruppb/GameModeOneActivity.kt
@@ -62,7 +62,10 @@ class GameMode1Activity : AppCompatActivity() {
                 }
                 dialog.dismiss()
             }
+
+
             dialog.show()
+            dialog.window?.setBackgroundDrawableResource(R.color.trans)
         })
     }
 
